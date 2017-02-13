2359648b5d80709ec02e247e10ded870260c8e5c
thereckshop <thereckshop@mac.com>
2017-02-13 12:03:10 -0800

    portfolio day 2 init

---
+++ scripts/projectdata.js
@@ -0,0 +1,25 @@
+'use strict'
+
+var projectData = [
+  {
+    name: 'Project 1',
+    description: 'This project 1',
+    image: 'img1.jpg',
+    link: '',
+    date: ''
+  },
+  {
+    name: 'Project 2',
+    description: 'This project 2',
+    image: 'img2',
+    link: '',
+    date: ''
+  },
+  {
+  name: 'Project 3',
+  description: 'This project 3',
+  image: 'img3',
+  link: '',
+  date: '',
+  }
+]
--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,11 @@
 
 var projects = [];
 
-function Project(name, description, image, link) {
+function Project(name, description, image, link, date) {
   this.name = name;
   this.description = description;
   this.image = image;
   this.link = link;
+  this.date = date;
 
 }
