a1ad5beb6c90a210b8bc92dd99ab3da85a7c6ec1
thereckshop <thereckshop@mac.com>
2017-02-15 12:19:58 -0800

    finished

--- scripts/project.js
+++ scripts/project.js
@@ -2,13 +2,28 @@
 
 var projects = [];
 
-function Project(name, description, image, link, date) {
-  this.name = name;
-  this.description = description;
-  this.image = image;
-  this.link = link;
-  this.date = date;
-
-  Project.prototype.toHtml = function() {
-  }
+function Project(options) {
+  this.name = options.name;
+  this.description = options.description;
+  this.image = options.image;
+  this.imageUrl = options.imageUrl;
+  this.link = options.link;
+  this.date = options.date;
 }
+
+Project.prototype.toHtml = function() {
+  var $newProject = $('div.template').clone();
+  $newProject.removeClass('template');
+  $newProject.find('h1').html(this.name);
+  $newProject.find('img').html(this.image).attr('src', this.imageUrl);
+  console.log($newProject);
+  return $newProject;
+};
+
+projectData.forEach(function(sectionObject) {
+  projects.push(new Project(sectionObject));
+});
+
+projects.forEach(function(a){
+  $('#project-display').append(a.toHtml());
+});
--- scripts/projectdata.js
+++ scripts/projectdata.js
@@ -4,7 +4,8 @@ var projectData = [
   {
     name: 'Project 1',
     description: 'This project 1',
-    image: 'img1.jpg',
+    image: 'project 1 image',
+    imageUrl: 'img1.jpg',
     link: '',
     date: ''
   },
@@ -12,6 +13,7 @@ var projectData = [
     name: 'Project 2',
     description: 'This project 2',
     image: 'img2',
+    imageUrl: '',
     link: '',
     date: ''
   },
@@ -19,7 +21,8 @@ var projectData = [
     name: 'Project 3',
     description: 'This project 3',
     image: 'img3',
+    imageUrl: '',
     link: '',
     date: '',
   }
-]
+];
