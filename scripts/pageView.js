ea09cb76750a4c5d3991f0fdefdaca7802f4935f
thereckshop <thereckshop@mac.com>
2017-02-18 20:51:28 -0800

    worked more on getting tabs to work

--- scripts/pageView.js
+++ scripts/pageView.js
@@ -1,7 +1,8 @@
-var pageView = {};
+'use strict'
 
-pageView.handleTabs = function() {
-  $('#about').on('click', function(event){
-    event.preventDefault();
-  })
-}
+$('main-nav:fist-child').on('click', 'li', function(){
+  $('#page-nav li').removeClass('active-nav-tab')//Removes the listed class from page-nav li
+  $(this).addClass('active-nav-tab');
+  $('#page-content section').hide();
+  $('#' + $(this).data('section')).show();
+});
